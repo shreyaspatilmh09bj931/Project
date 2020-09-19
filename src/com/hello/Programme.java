129ccf83670eabf384b1274a4f1399f305f0e305
ShreyasPatil <shreyas.p@saksoft.com>
2020-09-20 00:08:32 +0530

    added new project

---
+++ src/com/hello/Programme.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.hello;
+
+/**
+ * @author SAKSOFT052
+ *
+ */
+public class Programme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("Hello Hell");
+	}
+
+}
